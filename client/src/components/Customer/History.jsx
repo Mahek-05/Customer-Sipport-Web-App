3736113bfbac1c89a93993a667048f6b6235f9da
Mahek Gupta <kiaile2002@gmail.com>
2024-10-18 23:09:40 +0530

    logging chat id

--- client/src/components/Customer/History.jsx
+++ client/src/components/Customer/History.jsx
@@ -37,6 +37,7 @@ export default function History() {
       <ul className="space-y-2 ">
         {chatHistory.length > 0 ? (
           chatHistory.map((chat) => (
+            console.log("Chat ID:", chat.id);
             <li key={chat.id} className="flex justify-between border-b pb-2">
               <span>{chat.issueTitle || chat.text}</span>
               <Link to={`/customer/ticket/${chat.id}`} className="text-gray-500">
