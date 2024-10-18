db6412833f13eb41826ccb18d6361d8a894e8dae
Mahek Gupta <kiaile2002@gmail.com>
2024-10-18 22:16:03 +0530

    Update Chat.jsx

--- client/src/components/Customer/Chat.jsx
+++ client/src/components/Customer/Chat.jsx
@@ -3,7 +3,7 @@ import { useState, useEffect, useRef } from "react";
 import { useParams } from "react-router-dom";
 import io from "socket.io-client";
 
-const SOCKET_SERVER_URL = "http://localhost:3000";
+const SOCKET_SERVER_URL = import.meta.env.VITE_API_BASE_URL;
 
 export function Chat({ userData }) {
   const username = userData.customer.username;
