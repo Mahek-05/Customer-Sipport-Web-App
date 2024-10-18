6f1953484b189875c5dc9c530c11ef968ef4e1eb
mahek_gupta <kiaile2002@gmail.com>
2024-10-18 21:40:43 +0530

    changes

--- backend/lib/helpers/asyncHandler.js
+++ backend/lib/helpers/asyncHandler.js
@@ -2,14 +2,13 @@ const { MongoError } = require('mongodb');
 const { errorHandler } = require('./responseHandler');
 const { errorCodes } = require('../constants/errorCodes');
 const dal = require('../dal/dal');
-const { NODE_ENV } = require('../configs/server.config');
 
 exports.asyncHandler = (fn) => async (req, res, next) => {
   try {
     await fn(req, res, next);
     return undefined;
   } catch (error) {
-    if (NODE_ENV === 'dev') console.error(error);
+    console.error(error);
     console.error(
       `${(req.user && req.user.id) || 'common'} - ${error.message} - ${
         errorCodes[error.message] ? errorCodes[error.message].message : 'Unknown Error'
